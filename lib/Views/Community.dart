a9c5de0a1ca376acfc1b002e19d5392fd8ae0a06
bhawe <karmanibhaweh@gmail.com>
2024-01-06 14:08:46 +0500

    - Add Medication Feature - Add Community Discussion Feature

--- lib/Views/Community.dart
+++ lib/Views/Community.dart
@@ -6,22 +6,8 @@ import 'package:provider/provider.dart';
 import 'package:seizure_deck/data/comment_data.dart';
 import 'package:seizure_deck/providers/user_provider.dart';
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Community Discussion',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: DiscussionScreen(),
-    );
-  }
-}
+
 
 class DiscussionScreen extends StatefulWidget {
 
@@ -93,9 +79,42 @@ class _DiscussionScreenState extends State<DiscussionScreen> {
               itemCount: comments.length,
               itemBuilder: (context, index) {
                 final message = comments[index];
-                return ListTile(
-                  title: Text(comments[index].name),
-                  subtitle: Text(comments[index].comment),
+                UserProvider userProvider = Provider.of(context,listen: false);
+                int? uid = userProvider.uid;
+                // Check if the message sender is the current user
+                // bool isSender;
+                bool isSender = int.parse(message.uid) == uid;
+                // if(message.uid == uid)
+                return Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Align(
+                    alignment: isSender ? Alignment.centerRight : Alignment.centerLeft,
+                    child: Card(
+                      color: isSender ? Colors.blue : Colors.grey,
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              comments[index].name,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: isSender ? Colors.white : Colors.black,
+                              ),
+                            ),
+                            SizedBox(height: 4.0),
+                            Text(
+                              comments[index].comment,
+                              style: TextStyle(
+                                color: isSender ? Colors.white : Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                 );
               },
             ),
